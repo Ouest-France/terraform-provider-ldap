255209d1bfbc651131932de86ff171f8035d60c4
Pablo RUTH <contact@pablo-ruth.fr>
2021-02-24 17:17:40 +0100

    add datasource LDAP Group

---
+++ ldap/data_source_resource_ldap_group.go
@@ -0,0 +1,43 @@
+package ldap
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceLDAPGroup() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceLDAPGroupRead,
+
+		Schema: map[string]*schema.Schema{
+			"ou": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"members": &schema.Schema{
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	}
+}
+
+func dataSourceLDAPGroupRead(d *schema.ResourceData, m interface{}) error {
+	dn := fmt.Sprintf("CN=%s,%s", d.Get("name").(string), d.Get("ou").(string))
+
+	d.SetId(dn)
+
+	return resourceLDAPGroupRead(d, m)
+}
