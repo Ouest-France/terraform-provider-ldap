30ace8d2c9feee729a41e653e003c4e49981df29
wachach <walid.achach@canal-plus.com>
2022-01-26 18:13:20 +0100

    change env variable name

--- ldap/provider.go
+++ ldap/provider.go
@@ -21,12 +21,12 @@ func Provider() *schema.Provider {
 			"bind_user": {
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("BIND_USER", nil),
+				DefaultFunc: schema.EnvDefaultFunc("LDAP_USER", nil),
 				Description: "LDAP username",
 			},
 			"bind_password": {
 				Type:        schema.TypeString,
-				DefaultFunc: schema.EnvDefaultFunc("BIND_PASSWORD", nil),
+				DefaultFunc: schema.EnvDefaultFunc("LDAP_PASSWORD", nil),
 				Required:    true,
 				Description: "LDAP password",
 			},
